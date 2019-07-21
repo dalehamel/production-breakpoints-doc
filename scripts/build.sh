da0e012ceb425cebac79e1043ac6f1b4c90a8c49
Dale Hamel <dale.hamel@srvthe.net>
2019-07-21 18:56:10 -0400

    blah

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,8 @@ gem install bundler:2.0.1
 bundle install
 bundle exec rake report:init
 bundle exec rake report:publish
-cp output/doc.html index.html
+
+if [ "$1" == "CI" ];then
+  rm .gitignore
+  cp output/doc.html index.html
+fi
